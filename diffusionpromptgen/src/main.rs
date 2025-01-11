6893c91da0f004e156a35d5700c96b228cac3b0d
Muktabh Mayank <maco.anshu@gmail.com>
2025-01-11 22:22:37 +0530

    4th commit

--- diffusionpromptgen/src/main.rs
+++ diffusionpromptgen/src/main.rs
@@ -35,7 +35,7 @@ enum CameraAngle {
 
 // Create an enum called Look implementing Debug and variants Cinematic, UltraRealistic, FilmGrain, DramaticLighting, GenreHorror, GenreWestern, GenreFantasy, GenreRomantic, GenreAnimation, GenreManga, ActionScene, MotionBlur, DynamicAction, DynamicMotion
 #[derive(Debug,Clone)]
-enum Look {
+enum Cinematography {
     Cinematic, 
     UltraRealistic, 
     FilmGrain, 
@@ -104,7 +104,7 @@ struct FOOCUSDiffusionPromptApp{
     pub subject_attributes : Vec<(String,AttributeWeight)>,
     
     
-    pub cinematic_look : Option<Look>,
+    pub cinematic_look : Option<(&'static str,  egui::ImageSource<'static>,Cinematography)>,
     pub cinematic_look_weight : AttributeWeight,
     
 
@@ -173,6 +173,132 @@ impl eframe::App for FOOCUSDiffusionPromptApp {
 
             ui.horizontal(|ui| {
 
+                // adding upto 5 additional attrbutes
+                let attributes_label = ui.label(" You can add upto 5 attributes of the subject below (all optional) and mention their priority : ");
+            });
+
+            ui.horizontal(|ui| {
+
+                // Attribute 1
+                let mut attribute1 = String::from("");
+                let mut attribute1_weight = AttributeWeight::Low;
+                let attribute1_decription_label = ui.label(" Attribute1 [Optional] : ");
+                ui.text_edit_singleline(&mut attribute1)
+                    .labelled_by(attribute1_decription_label.id);
+                let attribute1_description_weight_label  = ui.label(" Subject Weightage : ");
+                ui.radio_value(&mut attribute1_weight, AttributeWeight::Low, "Low").labelled_by(attribute1_description_weight_label.id);
+                ui.radio_value(&mut attribute1_weight, AttributeWeight::High, "High").labelled_by(attribute1_description_weight_label.id);
+
+                if attribute1 != String::from(""){
+                    self.subject_attributes.push( (attribute1, attribute1_weight) );
+                }
+
+            });
+
+            ui.horizontal(|ui| {
+                // Similarly add Attribute 2
+                let mut attribute2 = String::from("");
+                let mut attribute2_weight = AttributeWeight::Low;
+                let attribute2_decription_label   = ui.label(" Attribute2 [Optional] : ");
+                ui.text_edit_singleline(&mut attribute2)
+                    .labelled_by(attribute2_decription_label.id);
+                let attribute2_description_weight_label   = ui.label(" Subject Weightage  :  ");
+                ui.radio_value(&mut attribute2_weight, AttributeWeight::Low, "Low").labelled_by(attribute2_description_weight_label.id);
+                ui.radio_value(&mut attribute2_weight, AttributeWeight::High, "High").labelled_by(attribute2_description_weight_label.id);
+                
+                if attribute2 != String::from(""){
+                    self.subject_attributes.push( (attribute2, attribute2_weight) );
+                }
+
+            });
+
+            ui.horizontal(|ui| {
+                // Similarly add Attribute 3
+                let mut attribute3 = String::from("");
+                let mut attribute3_weight = AttributeWeight::Low;
+                let attribute3_decription_label   = ui.label(" Attribute3 [Optional] : ");
+                ui.text_edit_singleline(&mut attribute3)
+                    .labelled_by(attribute3_decription_label.id);
+                let attribute3_description_weight_label   = ui.label(" Subject Weightage  :  ");
+                ui.radio_value(&mut attribute3_weight, AttributeWeight::Low, "Low").labelled_by(attribute3_description_weight_label.id);
+                ui.radio_value(&mut attribute3_weight, AttributeWeight::High, "High").labelled_by(attribute3_description_weight_label.id);
+
+                if attribute3 != String::from(""){
+                    self.subject_attributes.push( (attribute3, attribute3_weight) );
+                }
+
+            });
+
+            ui.horizontal(|ui| {
+                // Similarity add attribute 4 and 5
+                let mut attribute4 = String::from("");
+                let mut attribute4_weight  = AttributeWeight::Low;
+
+                let attribute4_decription_label    = ui.label(" Attribute4 [Optional] : ");
+                ui.text_edit_singleline(&mut attribute4)
+                  .labelled_by(attribute4_decription_label.id);
+                let attribute4_description_weight_label    = ui.label(" Subject Weightage   :   ");
+                ui.radio_value(&mut attribute4_weight, AttributeWeight::Low,"Low").labelled_by(attribute4_description_weight_label.id);
+                ui.radio_value(&mut attribute4_weight, AttributeWeight::High,"High").labelled_by(attribute4_description_weight_label.id);
+
+                if attribute4 != String::from(""){
+                    self.subject_attributes.push( (attribute4, attribute4_weight) );
+                }
+
+            });
+
+            ui.horizontal(|ui| {
+                let mut attribute5 = String::from("");
+                let mut attribute5_weight  = AttributeWeight::Low;  
+                let attribute5_decription_label    = ui.label(" Attribute5 [Optional] : ");
+                ui.text_edit_singleline(&mut attribute5)
+                  .labelled_by(attribute5_decription_label.id);
+                let attribute5_description_weight_label    = ui.label(" Subject Weightage   :   ");
+                ui.radio_value(&mut attribute5_weight, AttributeWeight::Low,"Low").labelled_by(attribute5_description_weight_label.id);
+                ui.radio_value(&mut attribute5_weight, AttributeWeight::High,"High").labelled_by(attribute5_description_weight_label.id);
+
+                if attribute5 != String::from(""){
+                    self.subject_attributes.push( (attribute5, attribute5_weight) );
+                }
+
+            });
+
+            let cinematography_vec = vec![
+               ("Cinematic",egui::include_image!("../assets/ferris.png"),Cinematography::Cinematic),
+               // Add new elements with the first being incrementing index, second being "../assets/ferris.png", third being from [   Cinematography::UltraRealistic, Cinematography::FilmGrain, Cinematography::DramaticLighting, Cinematography::GenreHorror, Cinematography::GenreWestern, Cinematography::GenreFantasy, Cinematography::GenreRomantic, Cinematography::GenreAnimation, Cinematography::GenreManga,  Cinematography::ActionScene, Cinematography::MotionBlur, Cinematography::DynamicAction,  Cinematography::DynamicMotion]
+               ("Ultra Realistic",egui::include_image!("../assets/ferris.png"),Cinematography::UltraRealistic),
+                ("Film Grain",egui::include_image!("../assets/ferris.png"),Cinematography::FilmGrain),
+                ("Dramatic Lighting",egui::include_image!("../assets/ferris.png"),Cinematography::DramaticLighting),
+                ("Horror",egui::include_image!("../assets/ferris.png"),Cinematography::GenreHorror),
+                ("Western",egui::include_image!("../assets/ferris.png"),Cinematography::GenreWestern),
+                ("Fantasy",egui::include_image!("../assets/ferris.png"),Cinematography::GenreFantasy),
+                ("Romantic",egui::include_image!("../assets/ferris.png"),Cinematography::GenreRomantic),
+                ("Animation",egui::include_image!("../assets/ferris.png"),Cinematography::GenreAnimation),
+                ("Manga",egui::include_image!("../assets/ferris.png"),Cinematography::GenreManga),
+                ("Action Scene",egui::include_image!("../assets/ferris.png"),Cinematography::ActionScene),
+                ("Motion Blur",egui::include_image!("../assets/ferris.png"),Cinematography::MotionBlur),
+                ("Dynamic Action",egui::include_image!("../assets/ferris.png"),Cinematography::DynamicAction),
+                ("Dynamic Motion",egui::include_image!("../assets/ferris.png"),Cinematography::DynamicMotion),
+            ];
+
+
+            let mut selected_cinematography_index :Option<usize>= None; 
+            // Create a button for each image option
+            for (index, (string_name,image_path,cinematography)) in cinematography_vec.iter().enumerate() {
+                if ui.button(string_name.clone()).clicked() {
+                    selected_cinematography_index = Some(index);
+                    self.cinematic_look = Some((string_name.clone(),image_path.clone(),cinematography.clone()));
+                }
+            }
+
+            // Display the selected image if available
+            if let Some(cinematic_look) = self.cinematic_look.clone() {
+                        ui.image(cinematic_look.1);
+            }
+   
+
+            ui.horizontal(|ui| {
+
                 let output_text = gen_prompt(self.clone());
                 let po = ui.label("Output::");
                 ui.label(output_text).labelled_by(po.id);
@@ -180,9 +306,9 @@ impl eframe::App for FOOCUSDiffusionPromptApp {
 
             });
 
-            ui.image(egui::include_image!(
-                "../assets/ferris.png"
-            ));
+            //ui.image(egui::include_image!(
+            //    "../assets/ferris.png"
+            //));
 
 
 
